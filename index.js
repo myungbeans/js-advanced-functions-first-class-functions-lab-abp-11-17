be8cf7869d58f25ac447b207bc5a43434f63bec8
Michael Myung <michael.h.myung@gmail.com>
2017-12-12 21:07:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,4 +18,6 @@ function createFareMultiplier(multiplier){
 const fareDoubler = createFareMultiplier(2)
 const fareTripler = createFareMultiplier(3)
 
-function selectDifferentDrivers (drivers)
+function selectDifferentDrivers (drivers){
+  
+}
