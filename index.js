2458afb12661a17f1a3c71ff35c7cf9711678757
Michael Myung <michael.h.myung@gmail.com>
2017-12-12 21:00:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const returnLastTwoDrivers = function(drivers){
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
 
 function createFareMultiplier(multiplier){
-  return function mult(multiplier){
+  return function ()){
     multiplier * multiplier
     }
   }
