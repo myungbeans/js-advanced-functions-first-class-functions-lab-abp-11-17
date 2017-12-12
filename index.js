09201271c8224a45dd210ff0a5d65c2079ca837c
Michael Myung <michael.h.myung@gmail.com>
2017-12-12 20:27:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const returnFirstTwoDrivers = function(drivers){
 }
 
 const returnLastTwoDrivers = function(drivers){
-  return [drivers[drivers.length - 1]
+  return [drivers[drivers.length - 1]]
 }
